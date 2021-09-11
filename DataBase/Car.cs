59fd202d1c61ea05b87b799c532b3fd3f98402d3
AlexeiEK <a3278cpp@yandex.ru>
2021-09-11 13:03:03 +0300

    Создал класс Car

--- DataBase/Car.cs
+++ DataBase/Car.cs
@@ -8,5 +8,12 @@ namespace DataBase
 {
     class Auto
     {
+        public int Id { get; set; }
+        public string RegSign { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public DateTime Year { get; set; }
+        public int Mileage { get; set; }
+        public string Colour { get; set; }
     }
 }
