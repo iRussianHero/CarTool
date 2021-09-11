574a98956919c96bc79f3e8d20cf3907a1fd53a6
Alexander Deyukhin <vyshky2@Gmail.com>
2021-09-11 12:53:58 +0400

    Реализовал Menu

---
+++ CLI/Show.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace CLI
+{
+    public class Show
+    {
+        public static void Menu()
+        {
+            Console.WriteLine("1. Действия с водителями");
+            Console.WriteLine("2. Действия с автомобилями");
+            Console.WriteLine("3. Действия с сервисом");
+            Console.WriteLine("4. Дполнительные функции");
+            var key = Convert.ToChar(Console.ReadLine());
+            switch (key)
+            {
+                case '1':
+                    SubMenu1();
+                    break;
+                case '2':
+                    SubMenu2();
+                    break;
+                case '3':
+                    SubMenu3();
+                    break;
+                case '4':
+                    SubMenu4();
+                    break;
+                default:
+                    Console.WriteLine("Введен некорректный символ");
+                    break;
+            }
+
+            static void SubMenu1()
+            {
+                Console.WriteLine("1 Показать всех водителей");
+                Console.WriteLine("2 Ввод данных водителя");
+                Console.WriteLine("3 Экспорт данных водителей");
+                Console.WriteLine("4 Импорт данных водителей");
+                var key = Convert.ToChar(Console.ReadLine());
+                switch (key)
+                {
+                    case '1':
+                        break;
+                    case '2':
+                        break;
+                    case '3':
+                        break;
+                    case '4':
+                        break;
+                    default:
+                        Console.WriteLine("Введен некорректный символ");
+                        break;
+                }
+            }
+
+            static void SubMenu2()
+            {
+                Console.WriteLine("1 Показать все автомобили");
+                Console.WriteLine("2 Ввод данных автомобиля");
+                Console.WriteLine("3 Экспорт данных автомобиля");
+                Console.WriteLine("4 Импорт данных автомобиляй");
+                var key = Convert.ToChar(Console.ReadLine());
+                switch (key)
+                {
+                    case '1':
+                        break;
+                    case '2':
+                        break;
+                    case '3':
+                        break;
+                    case '4':
+                        break;
+                    default:
+                        Console.WriteLine("Введен некорректный символ");
+                        break;
+                }
+            }
+            static void SubMenu3()
+            {
+                Console.WriteLine("1 Показать сервисную информацию");
+                Console.WriteLine("2 Ввод данных об обслуживании");
+                Console.WriteLine("3 Экспорт данных об обслуживании");
+                Console.WriteLine("4 Импорт данных об обслуживании");
+                var key = Convert.ToChar(Console.ReadLine());
+                switch (key)
+                {
+                    case '1':
+                        break;
+                    case '2':
+                        break;
+                    case '3':
+                        break;
+                    case '4':
+                        break;
+                    default:
+                        Console.WriteLine("Введен некорректный символ");
+                        break;
+                }
+            }
+            static void SubMenu4()
+            {
+                Console.WriteLine("1 Проверка состояния Автомобиля");
+                Console.WriteLine("2 Проверка пробега по Водителю");
+                var key = Convert.ToChar(Console.ReadLine());
+                switch (key)
+                {
+                    case '1':
+                        break;
+                    case '2':
+                        break;
+                    default:
+                        Console.WriteLine("Введен некорректный символ");
+                        break;
+                }
+            }
+            
+            
+        }
+    }
+}
