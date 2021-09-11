16720a8701ce28dd8b295b0941ff5a7cca00002f
AlexeiEK <a3278cpp@yandex.ru>
2021-09-11 12:56:59 +0300

    Добавил библиотеку DataBase и создал класс Driver

---
+++ DataBase/Driver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataBase
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public int DriveExp { get; set; }
+        public int IdCar { get; set; }
+    }
+}
