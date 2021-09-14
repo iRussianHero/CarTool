58c2ddae6b0d748591db1c504f8d4d06c3e1878f
Roman <irussianhero@gamil.com>
2021-09-14 21:54:10 +0400

    Мелкие изменения

--- DataBase/Insert.cs
+++ DataBase/Insert.cs
@@ -5,11 +5,12 @@ namespace DataBaseLib
 {
     public class Insert
     {
-        public static void Driver(Driver driver, ConnectionDB connection)
+        public static void Driver(List<Driver> list, ConnectionDB connection)
         {
             connection.Open();
-            var sql = $"INSERT INTO driver(full_name,age,driving_experience,id_auto)" +
-                      $"VALUES ('{driver.FullName}','{driver.Age}','{driver.DriveExp}','{driver.IdCar}');";
+            var sql = string.Empty;
+                sql = $"INSERT INTO driver(full_name,age,driving_experience,id_auto)" +
+                       $"VALUES ('{list[0].FullName}','{list[0].Age}','{list[0].DriveExp}','{list[0].IdCar}');";
             connection.command.CommandText = sql;
             connection.command.ExecuteNonQuery();
             connection.Close();
