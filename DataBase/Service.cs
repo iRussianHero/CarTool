af8339e7b76f23acb669b229409f69472337707a
Roman <irussianhero@gamil.com>
2021-09-15 21:49:44 +0400

    Добавил метод: Select Service

--- DataBase/Service.cs
+++ DataBase/Service.cs
@@ -9,7 +9,7 @@ namespace DataBaseLib
 {
     public class Service : IQuery
     {
-        public int Id { get; set; }
+        public uint Id { get; set; }
         public int IdCar { get; set; }
         public int Oil { get; set; }
         public bool ControlBreak { get; set; }
@@ -23,7 +23,32 @@ namespace DataBaseLib
 
         public IList Select(ConnectionDB connection)
         {
-            return new List<Car>();
+            connection.Open();
+            List<Service> list = new List<Service>();
+            var sql = $@"SELECT * FROM service";
+            connection.command.CommandText = sql;
+            var reader = connection.command.ExecuteReader();
+            if (!reader.HasRows) return null;
+            while (reader.Read())
+            {
+                var id = reader.GetUInt32("id");
+                var idAuto = reader.GetInt32("id_auto");
+                var oil = reader.GetInt32("oil");
+                var controlBreak = reader.GetBoolean("control_break");
+                var antifreeze = reader.GetInt32("antifreeze");
+                var tires = reader.GetBoolean("tires");
+                list.Add(new Service
+                {
+                    Id = id,
+                    IdCar = idAuto,
+                    Oil = oil,
+                    ControlBreak = controlBreak,
+                    Antifreeze = antifreeze,
+                    Tires = tires
+                });
+            }
+            connection.Close();
+            return new List<Service>();
         }
     }
 }
